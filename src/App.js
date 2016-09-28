f1099e4d98ddd32702eb684d106ea39b1445b7e9
Daniele Bertella <daniele.bertella@link-me.it>
2016-09-28 16:28:44 +0200

    add bower json and styleguides

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import reactPolymer from 'react-polymer';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -10,9 +11,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Simple note application layout</h1>
+
+        <tabs-clab vertical labels='["Today at 16:30", "Yesterday at 14:42", "Yesterday at 8:15"]'>
+          <div className="tab-content">
+            <p>Get things <b>done</b></p>
+          </div>
+        	<div className="tab-content">
+        	  <ul className="default">
+        	    <li>Buy milk</li>
+        	    <li>Clean desk</li>
+        	    <li>Call home</li>
+        	  </ul>
+        	</div>
+        	<div className="tab-content">
+        	  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique fuga voluptate ea, temporibus dolores quasi consequuntur libero.</p>
+        	</div>
+        </tabs-clab>
+
+        <button-clab block icon="icon-plus">New note</button-clab>
       </div>
     );
   }
